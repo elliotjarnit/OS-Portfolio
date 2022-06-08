5f0cd958b73df5a4c2a84229560745a49aa5fed7
Swiftzerr <swiftzermc@gmail.com>
2022-06-08 18:31:11 -0400

    blinking cursor

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,12 +2,33 @@
 
 <template>
   <main>
-    <div class="heading">
-      <h2 class="bg-">Hi! My name is,</h2>
-      <h1>Elliot</h1>
-      <h3>
-      an aspiring Web Developer
-      </h3>
+    <div class="absolute top-0 grid grid-cols-1 h-full w-full place-content-center">
+      <div class="text-center font-['Poppins'] text-3xl p-2 w-100">Hi! Im,</div>
+      <div class="text-center font-['Poppins'] text-7xl p-2 w-100">Elliot
+        <div class="relative inline right-2 bg-red-500" :style="{opacity: cursorShown ? '1' : '0'}">&nbsp;</div>
+      </div>
+      <div class="text-center font-['Poppins'] text-2xl p-2 w-100">An Aspiring Web Developer</div>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  mounted: function () {
+    this.timer = setInterval(() => {
+      this.cursorShown = !this.cursorShown;
+    }, 500)
+  },
+
+  data() {
+    return {
+      timer: null,
+      cursorShown: true
+    }
+  },
+
+  beforeDestroy() {
+    clearInterval(this.timer)
+  }
+}
+</script>
