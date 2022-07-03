b02f5ffe8924ebf2caeca1807571effd2480d05d
Swiftzerr <swiftzermc@gmail.com>
2022-07-03 09:01:38 -0400

    really removed quasar

--- src/main.js
+++ src/main.js
@@ -1,22 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./assets/base.scss";
-import { Quasar } from "quasar";
 import VueAnimateOnScroll from "vue-animate-onscroll";
 
 import App from "./App.vue";
 import router from "./router";
 
-import "@quasar/extras/material-icons/material-icons.css";
-import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
-import "@quasar/extras/material-icons-round/material-icons-round.css";
-import "quasar/dist/quasar.css";
-// import @quasar/extras/animate/fadeIn.css
-// import @quasar/extras/animate/fadeOut.css
-
 const app = createApp(App);
 
-app.use(Quasar, { plugins: {} });
 app.use(VueAnimateOnScroll);
 app.use(createPinia());
 app.use(router);
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ template: { transformAssetUrls } }), quasar(), vueJsx()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
