d696a1c135706c4f4988857bf38547836c6506b8
Swiftzerr <elliotjarnit@gmail.com>
2023-01-11 14:18:18 -0800

    Beautiful splash screen 🤌

---
+++ src/routes/+page.svelte
@@ -0,0 +1,149 @@
+<div class="page">
+  <div class="splash">
+    <div class="loginpage">
+      <img class="pfp" src="/pfp.jpeg" alt="profile picture" />
+      <p>Elliot Jarnit</p>
+      <input type="password" placeholder='Enter Password' value={passwordfieldval} class="passwordfield" on:keydown|preventDefault={void(0)} />
+    </div>
+  </div>
+  <img src='/cursor.png' alt="cursor" class="cursor" />
+  <div class="main" style="display: none; opacity: 0;">
+    <h1>🚧 Site Under Construction 🚧</h1>
+  </div>
+</div>
+
+<script>
+import { onMount } from 'svelte';
+
+let passwordfieldval = ""
+
+onMount(() => {
+  const cursor = document.querySelector('.cursor');
+  const passwordfield = document.querySelector('.passwordfield');
+  const splash = document.querySelector('.splash');
+  const main = document.querySelector('.main');
+
+  // Wait 1 second
+  setTimeout(() => {
+    // Move cursor to the password field
+    cursor.style.transform = "translate(-50px, 125px)"
+    setTimeout(async () => {
+      // Move cursor to the password field
+      passwordfield.focus()
+      await new Promise(resolve => setTimeout(resolve, 950));
+      let count = 0
+      while (count < 15) {
+        await new Promise(r => setTimeout(r, Math.trunc(Math.random() * 350)));
+        passwordfield.value += "a"
+        count++
+      }
+      await new Promise(resolve => setTimeout(resolve, 900));
+      cursor.style.transition = "transform 1.5s"
+      // Move cursor to the login button
+      cursor.style.transform = "translate(73px, 129px)"
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      splash.style.opacity = "0"
+      cursor.style.opacity = "0"
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      splash.style.display = "none"
+      main.style.display = "flex"
+      main.style.opacity = "1"
+
+    }, 3200);
+  }, 1000);
+})
+</script>
+
+<style lang="scss">
+  @font-face {
+    font-family: "SFPro";
+    src: url("/SFProRegular.OTF");
+  }
+
+  .page {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-image: url("https://4kwallpapers.com/images/wallpapers/macos-13-macos-ventura-macos-2022-stock-light-5k-retina-1920x1080-8135.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    font-family: SFPro,sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main {
+    transition: opacity 1s;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .cursor {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    pointer-events: none;
+    object-fit: contain;
+    transform: translate(130px, calc(50vh + 30px));
+    transition: transform 3s, opacity 2s;
+  }
+
+  .splash {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 2s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .loginpage {
+      user-select: none;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .pfp {
+        border-radius: 50%;
+        width: 175px;
+        height: 175px;
+        margin-bottom: 0.4em;
+      }
+
+      p {
+        color: white;
+        text-shadow: 0 0 0.3em #000000;
+        font-size: 1.25em;
+        margin-bottom: 0.5em;
+      }
+
+      .passwordfield {
+        color: black;
+        opacity: 0.9;
+        border-radius: 15px;
+        outline: none;
+        border: none;
+        padding: 0.3em 13px;
+        background-color: rgba(255, 255, 255, 0.3);
+        box-shadow: 0 25.6px 57.6px rgb(0 0 0 / 14%), 0px 0px 16.4px rgb(0 0 0 / 12%);
+        backdrop-filter: blur(40px);
+        -webkit-backdrop-filter: blur(40px);
+      }
+
+      .passwordfield:not(:placeholder-shown) {
+        background-image: url("/arrow-icon.png");
+        background-size: 17px;
+        background-repeat: no-repeat;
+        background-position: 141px 3px;
+        vertical-align: middle;
+      }
+    }
+  }
+</style>
