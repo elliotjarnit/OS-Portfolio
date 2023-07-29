b0d706cbe08842bea272c76bff031ae23c8477d0
Swiftzerr <swiftzermc@gmail.com>
2023-07-29 14:38:53 -0700

    remove api3

--- server/api/images/upload.post.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async (event) => {
-  throw createError({
-    statusCode: 503,
-    name: 'NotImplementedError',
-    message: 'This endpoint is not implemented yet.',
-    statusMessage: "Not Implemented",
-  })
-});
