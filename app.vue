6de1ced966dcb018a59aa13f3ff92d9913f61d34
elliotjarnit <elliotjarnit@gmail.com>
2023-08-06 20:11:08 -0600

    banner

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="banner">
+    <span>This website is still heavily under construction and is far from finished</span>
+  </div>
   <div>
     <NuxtPage />
   </div>
@@ -8,5 +11,26 @@
 body {
   margin: 0;
   overflow: hidden;
+}
+
+.banner {
+position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 40px;
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  font-size: 0.8rem;
+  font-weight: 600;
+  color: #333;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.banner span {
+  margin: 0 5px;
 }
 </style>
