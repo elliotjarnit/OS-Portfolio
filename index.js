14ae0b3352e57776b714cc40f1cdfdf92b07b6ce
elliotjarnit <me@elliotjarnit.dev>
2024-09-19 10:49:10 -0700

    Redo

---
+++ index.js
@@ -0,0 +1,47 @@
+const icons = ["bun", "cloudflare", "cplusplus", "css3", "electron", "express", "figma", "firebase", "framermotion", "git", "github", "go", "googlecloud", "gradle", "graphql", "html5", "java", "javascript", "kotlin", "lua", "mongodb", "mysql", "nextjs", "nodejs", "nuxtjs", "objectivec", "openapi", "opengl", "opencl", "php", "postgresql", "prisma", "python", "react", "redis", "rust", "sass", "socketio", "sqlite", "supabase", "svelte", "swift", "tailwindcss", "tauri", "tensorflow", "threejs", "typescript", "vuejs"]
+
+function pickIcons(count) {
+  const pickedIcons = []
+  const availableIcons = [...icons]
+
+  for (let i = 0; i < count; i++) {
+    const index = Math.floor(Math.random() * availableIcons.length)
+    pickedIcons.push(availableIcons[index])
+    availableIcons.splice(index, 1)
+  }
+
+  return pickedIcons
+}
+
+function populateIcons() {
+  const circle1 = document.getElementById("circle-1")
+  const circle2 = document.getElementById("circle-2")
+
+  const icons = pickIcons(24)
+
+  circle1.innerHTML = ""
+  circle2.innerHTML = ""
+
+  icons.forEach((icon, index) => {
+    const circle = index < 8 ? circle1 : circle2
+    const itemCount = index < 8 ? 8 : 16
+    circle.innerHTML += `<img class="dev-icon" src="icons/${icon}.svg" alt="${icon}" style="--rot: ${index * (360 / itemCount)}" />`
+  })
+}
+
+function startAnimation() {
+  const circle1 = document.getElementById("circle-1")
+  const circle2 = document.getElementById("circle-2")
+
+  let offset = 0;
+
+  setInterval(() => {
+    offset++;
+    circle1.style.setProperty("--offset", offset)
+    circle2.style.setProperty("--offset", offset)
+
+  }, 100)
+}
+
+populateIcons()
+startAnimation();
--- plugins/analytics.client.ts
+++
@@ -1,6 +0,0 @@
-import { inject } from '@vercel/analytics';
-
-export default defineNuxtPlugin((NuxtApp) => {
-  // Inject Vercel analytics
-  inject();
-})
